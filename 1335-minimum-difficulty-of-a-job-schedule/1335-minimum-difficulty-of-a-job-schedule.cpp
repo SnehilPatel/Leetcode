2b2e3f2d10bf291f2a9fb28b25e9e6687c4bc132
Snehil Patel <53311202+SnehilPatel@users.noreply.github.com>
2022-10-16 11:56:33 +0530

    Time: 50 ms (77.92%), Space: 7.3 MB (93.10%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int dp[301][11];
+    int solve(int i, int n, int j, vector<int>&num){
+        if(j < 0)
+            return 1e9;
+        if(i == n)
+        {
+            if(j != 0)
+                return 1e9;
+            return 0;
+        }
+        if(dp[i][j] != -1) 
+            return dp[i][j];
+        
+        int maxi = INT_MIN;
+        int res = INT_MAX;
+        for(int curri = i; curri < n; curri++)
+        {
+            maxi = max(maxi, num[curri]);
+            res = min(res, maxi+solve(curri+1, n, j-1, num));
+        }
+        return dp[i][j] = res;
+    }
+    int minDifficulty(vector<int>& nums, int d) {
+        int n = nums.size();
+        if(n < d)
+            return -1;
+        
+        memset(dp, -1, sizeof(dp));
+        int ans = solve(0, n, d, nums);
+        
+        if(ans >= 1e9)
+            return -1;
+        return ans;        
+    }
+};
