3b9abed9736461655a41d5af22a38299c2ecaadc
Snehil Patel <53311202+SnehilPatel@users.noreply.github.com>
2022-10-23 11:12:19 +0530

    Time: 16 ms (37.55%), Space: 11.4 MB (49.60%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solve(string s,vector<string> &ans,int i){
+        if(i==s.size()){
+            ans.push_back(s);
+            return;
+        }
+        if(s[i]>='0' and s[i]<='9')
+            solve(s,ans,i+1);
+        else{
+            s[i] = tolower(s[i]);
+            solve(s,ans,i+1);
+            s[i] = toupper(s[i]);
+            solve(s,ans,i+1);
+        }
+    }
+
+    vector<string> letterCasePermutation(string s) {
+
+        vector<string> ans;
+        solve(s,ans,0);
+        return ans;
+
+    }
+};
