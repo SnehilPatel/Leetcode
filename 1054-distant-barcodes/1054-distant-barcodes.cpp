309a5c9165f85b1cc42052ad6c97c3dc47c33aca
Snehil Patel <53311202+SnehilPatel@users.noreply.github.com>
2022-11-03 19:59:09 +0530

    Time: 333 ms (7.19%), Space: 56.6 MB (5.08%) - LeetHub

---
+++ 1054-distant-barcodes/1054-distant-barcodes.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> rearrangeBarcodes(vector<int>& barcodes) {
+        if(barcodes.size() == 1)
+            return barcodes;
+        unordered_map<int, int> m;
+        for(auto x : barcodes){
+            m[x]++;
+        }
+        
+        priority_queue<pair<int, int>> pq;
+        for(auto x : m){
+            pq.push({x.second, x.first});
+        }
+        
+        vector<int> ans;
+        while(!pq.empty()){
+            vector<pair<int, int>> t;
+            for(int i = 0; i < 2; i++){
+                if(!pq.empty()){
+                    t.push_back(pq.top());
+                    pq.pop();
+                }
+            }
+            for(auto x : t){
+                if(x.first != 0){
+                    ans.push_back(x.second);
+                    x.first = x.first - 1;
+                    // if(x.first != 0){
+                    //     pq.push(x);
+                    // }
+                    pq.push(x);
+                }
+            }
+        }
+        return ans;
+    }
+};
